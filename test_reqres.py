16ebaf553e61e6e3d32ac7951556899662e5bfda
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2023-12-07 16:12:21 +0300

    part 2

--- test_reqres.py
+++ test_reqres.py
@@ -1,16 +1,32 @@
+import json
+import logging
+
+import allure
 import jsonschema
 import pytest
 import requests
+from allure_commons._allure import step
+from allure_commons.types import AttachmentType
 from requests import Response
 
 from utils import load_schema
 
 
+def reqres_api_get(url, **kwargs):
+    with step("API Request"):
+        result = requests.get(url="https://reqres.in" + url, **kwargs)
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
+        logging.info(result.request.url)
+        logging.info(result.status_code)
+        logging.info(result.text)
+    return result
+
+
 def test_get_single_user_successfully():
-    url = "https://reqres.in/api/users/2"
+    url = "/api/users/2"
     schema = load_schema("get_single_user.json")
 
-    result: Response = requests.get(url)
+    result: Response = reqres_api_get(url)
 
     assert result.status_code == 200
     jsonschema.validate(result.json(), schema)
@@ -18,9 +34,9 @@ def test_get_single_user_successfully():
 
 @pytest.mark.parametrize('id_', [1, 2, 3])
 def test_get_single_user_id(id_):
-    url = f"https://reqres.in/api/users/{id_}"
+    url = f"/api/users/{id_}"
 
-    result = requests.get(url)
+    result = reqres_api_get(url)
     assert result.json()['data']['id'] == id_
 
 
@@ -37,11 +53,11 @@ def test_list_of_users_per_page():
     page = 2
     per_page = 6
     url = "https://reqres.in/api/users"
-
-    result = requests.get(
-        url=url,
-        params={"page": page, "per_page": per_page}
-    )
-
+    with step("API Request"):
+        result = requests.get(
+            url=url,
+            params={"page": page, "per_page": per_page}
+        )
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON, extension="json")
     assert result.json()["per_page"] == per_page
     assert len(result.json()['data']) == per_page
